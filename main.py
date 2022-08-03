3e2de38cceb1266609e47f5534e31d02c8218942
Elijah Simon <elijahsimon@Elijahs-MacBook-Pro.local>
2022-08-03 18:59:44 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import plotly.express as px
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv("chd6.csv")
+
+fig = px.sunburst(
+    data_frame=df,
+    path=["Disease", "Category", 'Symptom', "Type"], # Root, branches, leaves
+    color="Category",
+    color_discrete_sequence=px.colors.qualitative.Pastel,
+    # maxdepth=-1,                        # set the sectors rendered. -1 will render all levels in the hierarchy
+    # color="Victim's age",
+    # color_continuous_scale=px.colors.sequential.BuGn,
+    # range_color=[10,100],
+
+    # branchvalues="total",               # or 'remainder'
+    hover_name="Prevalence",
+    hover_data={'Category': False},    # remove column name from tooltip  (Plotly version >= 4.8.0)
+    # title="Signs and Symptoms of ASXL1-Related Disorders",
+    # template='plotly',               # 'ggplot2', 'seaborn', 'simple_white', 'plotly',
+    #                                   # 'plotly_white', 'plotly_dark', 'presentation',
+    #                                   # 'xgridoff', 'ygridoff', 'gridon', 'none'
+)
+
+fig.update_traces(textinfo='label+percent parent')
+fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
+
+fig.show()
